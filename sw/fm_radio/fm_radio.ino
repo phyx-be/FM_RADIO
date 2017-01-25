7f234720f83fd766a9c45bbec490803bf28fb358
Bert Outtier <outtierbert@gmail.com>
2017-01-25 20:35:44 +0100

    First working version tuned at stubru

--- sw/fm_radio/fm_radio.ino
+++ sw/fm_radio/fm_radio.ino
@@ -1,36 +1,8 @@
+/// An Arduino sketch to operate a RDA5807M chip based radio using the Radio library of Matthias Hertel, http://www.mathertel.de.
+/// This sketch implements a fixed radio station. The volume can be controlled using a rotary encoder.
 ///
-/// \file  TestRDA5807M.ino
-/// \brief An Arduino sketch to operate a SI4705 chip based radio using the Radio library.
-///
-/// \author Matthias Hertel, http://www.mathertel.de
-/// \copyright Copyright (c) 2014 by Matthias Hertel.\n
-/// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
-///
-/// \details
-/// This sketch implements a "as simple as possible" radio without any possibility to modify the settings after initializing the chip.\n
-/// The radio chip is initialized and setup to a fixed band and frequency. These settings can be changed by modifying the 
-/// FIX_BAND and FIX_STATION definitions. 
-///
-/// Open the Serial console with 57600 baud to see the current radio information.
-///
-/// Wiring
-/// ------ 
-/// The RDA5807M board/chip has to be connected by using the following connections:
-/// | Arduino UNO pin    | Radio chip signal  | 
-/// | -------------------| -------------------| 
-/// | 3.3V (red)         | VCC                | 
-/// | GND (black)        | GND                | 
-/// | A5 or SCL (yellow) | SCLK               | 
-/// | A4 or SDA (blue)   | SDIO               | 
-/// The locations of the pins on the UNO board are written on the PCB.
-/// The locations of the signals on the RDA5807M side depend on the board you use.
-///
-/// More documentation and source code is available at http://www.mathertel.de/Arduino
-///
-/// ChangeLog:
-/// ----------
-/// * 05.12.2014 created.
-/// * 19.05.2015 extended.
+/// Bert Outtier
+/// Copyright (c) 2017 by Bert Outtier.
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -38,18 +10,26 @@
 #include <RDA5807M.h>
 
 // ----- Fixed settings here. -----
+#define FIX_BAND          RADIO_BAND_FM   ///< The band that will be tuned by this sketch is FM.
+#define FIX_STATION       8800            ///< The station that will be tuned by this sketch is 89.30 MHz.
+#define AMP_ENABLE_PIN    A0              ///< Amp enable pin (A0)
+#define ENCODER_A         A2              ///< Encoder pin A (A1)
+#define ENCODER_B         A1              ///< Encoder pin B (A2)
+#define ENCODER_BUTTON    A3              ///< Encoder button (A3)
+#define VOLUME_LEVELS     16
+#define PULSE_PER_VOLUME  1
 
-#define FIX_BAND     RADIO_BAND_FM   ///< The band that will be tuned by this sketch is FM.
-#define FIX_STATION  8930            ///< The station that will be tuned by this sketch is 89.30 MHz.
-#define ENCODER_A       1            ///< Encoder pin A (A1)
-#define ENCODER_B       2            ///< Encoder pin B (A2)
-#define ENCODER_BUTTON  3            ///< Encoder button (A3)
-#define VOLUME_LEVELS   16
-#define PULSE_PER_VOLUME  3
-
-int encoder0Pos = 0;
-int encoder0PinALast = LOW;
-int currentVolume = 0;
+int encoder0Pos = 0;            // encoder value (0 to VOLUME_LEVELS*PULSE_PER_VOLUME)
+int encoder0PinALast = LOW;     // previous value of encoder pin A
+int currentVolume = 0;          // Current volume level
+int previousAmpState = LOW;     // previous amp state (enabled/disabled)
+int currentAmpState = LOW;     // current amp state (enabled/disabled)
+int previousButtonPinState = LOW;  // previous button state (pressed/not pressed)
+int currentButtonState = LOW;
+int previousRadioState = HIGH;  // debounced previous button state (pressed/not pressed)
+int currentRadioState = HIGH;   // is the radio on or off?
+long time = 0;                  // the last time the encoder button pin was toggled
+long debounce = 100;            // the debounce time
 
 RDA5807M radio;    // Create an instance of Class for RDA5807M Chip
 
@@ -57,15 +37,23 @@ RDA5807M radio;    // Create an instance of Class for RDA5807M Chip
 /// with some debugging on the Serial port
 void setup() {
   // open the Serial port
-  Serial.begin(57600);
+  Serial.begin(9600);
   Serial.println("Radio...");
   delay(200);
+
+  //setup encoder
+  pinMode (AMP_ENABLE_PIN, OUTPUT);
+  pinMode (ENCODER_A,INPUT_PULLUP);
+  pinMode (ENCODER_B,INPUT_PULLUP);
+  pinMode (ENCODER_BUTTON,INPUT_PULLUP);
+
+  digitalWrite(AMP_ENABLE_PIN, currentAmpState);
 
   // Initialize the Radio 
   radio.init();
 
   // Enable information to the Serial port
-  radio.debugEnable();
+  //radio.debugEnable();
 
   // Set all radio setting to the fixed values.
   radio.setBandFrequency(FIX_BAND, FIX_STATION);
@@ -73,25 +61,23 @@ void setup() {
   radio.setMono(false);
   radio.setMute(false);
 
-  //setup encoder
-  pinMode (ENCODER_A,INPUT);
-  pinMode (ENCODER_B,INPUT);
+  Serial.println("Init done!");
   
 } // setup
 
 
 /// show the current chip data every 3 seconds.
 void loop() {
-  char s[12];
-  radio.formatFrequency(s, sizeof(s));
-  Serial.print("Station:"); 
-  Serial.println(s);
-  
-  Serial.print("Radio:"); 
-  radio.debugRadioInfo();
-  
-  Serial.print("Audio:"); 
-  radio.debugAudioInfo();
+//  char s[12];
+//  radio.formatFrequency(s, sizeof(s));
+//  Serial.print("Station:"); 
+//  Serial.println(s);
+//  
+//  Serial.print("Radio:"); 
+//  radio.debugRadioInfo();
+//  
+//  Serial.print("Audio:"); 
+//  radio.debugAudioInfo();
 
   int n = digitalRead(ENCODER_A);
   if ((encoder0PinALast == LOW) && (n == HIGH)) {
@@ -100,11 +86,62 @@ void loop() {
     } else {
       if(encoder0Pos < (VOLUME_LEVELS*PULSE_PER_VOLUME)) encoder0Pos++;
     }
+    Serial.print("Set encoder pos ");
+    Serial.println(encoder0Pos, DEC);
   } 
   encoder0PinALast = n;
+  
   if((encoder0Pos/PULSE_PER_VOLUME) != currentVolume) {
     currentVolume = encoder0Pos/PULSE_PER_VOLUME;
+    Serial.print("Set volume to ");
+    Serial.println(currentVolume, DEC);
     radio.setVolume(currentVolume);
+  }
+
+    // read the state of the switch into a local variable:
+  int reading = digitalRead(ENCODER_BUTTON);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != previousButtonPinState) {
+    // reset the debouncing timer
+    time = millis();
+  }
+
+  if ((millis() - time) > debounce) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != currentButtonState) {
+      currentButtonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (currentButtonState == HIGH) {
+        currentRadioState = !currentRadioState;
+        Serial.print("Set radio state: ");
+        Serial.println(currentRadioState, DEC);
+      }
+    }
+  }
+  previousButtonPinState = reading;
+  
+  if(currentVolume == 0 || !currentRadioState){
+    currentAmpState = LOW;
+  }
+
+  if(currentVolume > 0 && currentRadioState){
+    currentAmpState = HIGH;
+  }
+
+  if(previousAmpState != currentAmpState){
+    Serial.print("Set amp: ");
+    Serial.println(currentAmpState, DEC);
+    digitalWrite(AMP_ENABLE_PIN, currentAmpState);
+    previousAmpState = currentAmpState;
   }
 } // loop
 
